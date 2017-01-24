76938f2290060842bff6d22febfac949628f65bb
Martin Crane <martin.crane@gmail.com>
2017-01-24 00:46:57 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,32 +17,38 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
+
+  holiday_hash[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above 
-  # add the second argument, which is a supply, to BOTH the 
+  # holiday_hash is identical to the one above
+  # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  
+  holiday_hash[:winter][:christmas]<<supply
+  holiday_hash[:winter][:new_years]<<supply
 end
- 
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  holiday_hash[:spring][:memorial_day]<<supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  holiday_hash[:winter].values.flatten
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +59,31 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize + ":"
+      holiday.each do |holy, supply|
+        puts "  " + holy.to_s.split("_").map {|v| v.capitalize}.join(" ")   + ": " + supply.join(", ")
+      end
+  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
+
+  bbq_chicks = []
+
+  holiday_hash.each do |season, holiday|
+  	holiday.each do |holy, supplies|
+	  	if supplies.include? "BBQ"
+	  		bbq_chicks.push(holiday.keys[0])
+	  	else
+	  	end
+	 end
+  end
+
+  bbq_chicks
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
